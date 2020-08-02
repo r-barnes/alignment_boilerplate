7595df188360ebc71763ddefb92858bdb581c443
Richard Barnes <rbarnes@umn.edu>
2020-08-01 20:32:28 -0600

    Improve unique pointers and make some special range-based host copies

--- include/albp/memory.hpp
+++ include/albp/memory.hpp
@@ -1,32 +1,13 @@
 #pragma once
 
-#include "error_handling.hpp"
+#include <albp/error_handling.hpp>
+#include <albp/ranges.hpp>
 
 #include <iostream>
 #include <memory>
 #include <string>
 
 namespace albp {
-
-//Allocate `count` items in page-locked memory
-template<class T>
-T* PageLockedMalloc(const size_t count, const T *const host_data=nullptr){
-    T *temp;
-    ALBP_CUDA_ERROR_CHECK(cudaMallocHost(&temp, count*sizeof(T), cudaHostAllocDefault));
-    if(host_data)
-        ALBP_CUDA_ERROR_CHECK(cudaMemcpy(temp, host_data, count*sizeof(T), cudaMemcpyHostToHost));
-    return temp;
-}
-
-//Allocate `count` items on device memory
-template<class T>
-T* DeviceMalloc(const size_t count, const T *const host_data=nullptr){
-    T *temp;
-    ALBP_CUDA_ERROR_CHECK(cudaMalloc(&temp, count*sizeof(T)));
-    if(host_data)
-        ALBP_CUDA_ERROR_CHECK(cudaMemcpy(temp, host_data, count*sizeof(T), cudaMemcpyHostToDevice));
-    return temp;
-}
 
 template<typename T>
 struct cuda_device_deleter {
@@ -49,5 +30,69 @@ using cuda_unique_hptr = std::unique_ptr<T[], cuda_host_deleter<T>>;
 
 template<class T>
 using cuda_unique_dptr = std::unique_ptr<T[], cuda_device_deleter<T>>;
+
+//Allocate `count` items in page-locked memory
+template<class T>
+T* PageLockedMalloc(const size_t count, const T *const host_data=nullptr){
+    T *temp;
+    ALBP_CUDA_ERROR_CHECK(cudaMallocHost(&temp, count*sizeof(T), cudaHostAllocDefault));
+    if(host_data)
+        ALBP_CUDA_ERROR_CHECK(cudaMemcpy(temp, host_data, count*sizeof(T), cudaMemcpyHostToHost));
+    return temp;
+}
+
+//Allocate `count` items in page-locked memory
+template<class T>
+cuda_unique_hptr<T> PageLockedMallocUnique(const size_t count, const T *const host_data=nullptr){
+    T *temp;
+    ALBP_CUDA_ERROR_CHECK(cudaMallocHost(&temp, count*sizeof(T), cudaHostAllocDefault));
+    if(host_data)
+        ALBP_CUDA_ERROR_CHECK(cudaMemcpy(temp, host_data, count*sizeof(T), cudaMemcpyHostToHost));
+    return cuda_unique_hptr<T>(temp);
+}
+
+//Allocate `count` items on device memory
+template<class T>
+T* DeviceMalloc(const size_t count, const T *const host_data=nullptr){
+    T *temp;
+    ALBP_CUDA_ERROR_CHECK(cudaMalloc(&temp, count*sizeof(T)));
+    if(host_data)
+        ALBP_CUDA_ERROR_CHECK(cudaMemcpy(temp, host_data, count*sizeof(T), cudaMemcpyHostToDevice));
+    return temp;
+}
+
+//Allocate `count` items on device memory
+template<class T>
+cuda_unique_dptr<T> DeviceMallocUnique(const size_t count, const T *const host_data=nullptr){
+    T *temp;
+    ALBP_CUDA_ERROR_CHECK(cudaMalloc(&temp, count*sizeof(T)));
+    if(host_data)
+        ALBP_CUDA_ERROR_CHECK(cudaMemcpy(temp, host_data, count*sizeof(T), cudaMemcpyHostToDevice));
+    return cuda_unique_dptr<T>(temp);
+}
+
+///Copies `count` items from `host_ptr` to `dev_ptr` asynchronously
+template<class T>
+cudaError_t copy_to_device_async(T *dev_ptr, const T *host_ptr, const size_t count, const cudaStream_t stream){
+  return cudaMemcpyAsync(dev_ptr, host_ptr, count*sizeof(T), cudaMemcpyHostToDevice, stream);
+}
+
+///Copies `count` items from `dev_ptr` to `host_ptr` asynchronously
+template<class T>
+cudaError_t copy_to_host_async(T *host_ptr, const T *dev_ptr, const size_t count, const cudaStream_t stream){
+  return cudaMemcpyAsync(host_ptr, dev_ptr, count*sizeof(T), cudaMemcpyDeviceToHost, stream);
+}
+
+///Copies `count` items from `host_ptr` to `dev_ptr` asynchronously
+template<class T>
+cudaError_t copy_to_device_async(T *dev_ptr, const T *host_ptr, const RangePair &rp, const cudaStream_t stream){
+  return cudaMemcpyAsync(dev_ptr, &host_ptr[rp.begin], rp.size()*sizeof(T), cudaMemcpyHostToDevice, stream);
+}
+
+///Copies `count` items from `dev_ptr` to `host_ptr` asynchronously
+template<class T>
+cudaError_t copy_to_host_async(T *host_ptr, const T *dev_ptr, const RangePair &rp, const cudaStream_t stream){
+  return cudaMemcpyAsync(&host_ptr[rp.begin], dev_ptr, rp.size()*sizeof(T), cudaMemcpyDeviceToHost, stream);
+}
 
 }
