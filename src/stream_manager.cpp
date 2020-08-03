209a7104cb2a466b93ef0d05c78ecb4ac1bdd504
Richard Barnes <rbarnes@umn.edu>
2020-08-02 22:12:24 -0600

    Simplify Stream Manager

--- src/stream_manager.cpp
+++ src/stream_manager.cpp
@@ -1,32 +1,49 @@
+#include <albp/ctpl.hpp>
 #include <albp/error_handling.hpp>
+#include <albp/ranges.hpp>
 #include <albp/stream_manager.hpp>
+
+#include <cuda_runtime.h>
+
+#include <thread>
+#include <chrono>
+#include <iostream>
 
 namespace albp {
 
-GPUManager::GPUManager(
-  const std::vector<gpu_id_t> &gpu_ids,
-  const size_t streams_per_gpu,
-  const size_t thread_pool_size
-) : pool(thread_pool_size) {
-  for(const auto &gpu_id: gpu_ids)
-  for(size_t si=0;si<streams_per_gpu;si++){
-    gpu_streams[gpu_id].push_back(make_stream(gpu_id));
-  }
-}
-
-
-GPUManager::~GPUManager(){
-  for(const auto &gpu_stream: gpu_streams)
-  for(const auto &stream: gpu_stream.second)
-    ALBP_CUDA_ERROR_CHECK(cudaStreamDestroy(stream));
-}
-
-
-cudaStream_t GPUManager::make_stream(const gpu_id_t gpu_id) const {
+cudaStream_t get_new_stream(const int device_id){
+  ALBP_CUDA_ERROR_CHECK(cudaSetDevice(device_id));
   cudaStream_t temp;
-  ALBP_CUDA_ERROR_CHECK(cudaSetDevice(gpu_id));
   ALBP_CUDA_ERROR_CHECK(cudaStreamCreate(&temp));
   return temp;
+}
+
+
+
+void process_streams(StreamVector &sv, const size_t item_count, const size_t chunk_size){
+  process_streams(sv, RangePair(0, item_count), chunk_size);
+}
+
+void process_streams(StreamVector &sv, const RangePair range, const size_t chunk_size){
+  //One thread per stream
+  ctpl::thread_pool pool(sv.size());
+
+  //Break up the input range into chunks
+  const auto chunks = generate_chunks(range, 1);
+
+  std::cerr<<"chunks = "<<chunks.size()<<std::endl;
+
+  //Assign chunks to the streams round-robin style
+  for(size_t i=0;i<chunks.size();i++){
+    pool.push([&](const int){
+      sv.at(i%sv.size())(chunks.at(i));
+    });
+  }
+
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+
+  //Wait for all the tasks to finish and then close out the threads
+  pool.stop(true);
 }
 
 }
